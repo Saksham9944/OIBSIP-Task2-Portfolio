291fedd66b613e97c0948e84045f5d626a17a67b
Saksham9944 <128057183+Saksham9944@users.noreply.github.com>
2023-08-14 22:28:03 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+let type_words = new Typed(".multi-text", {
+  strings: ["Frontend Developer", "Java Programmer", "Problem Solver"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backdelay: 1000,
+  loop: true,
+});
